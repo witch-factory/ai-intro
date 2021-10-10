598c724462931c2b21c1ecb56e5f6cb0378da71f
witch-factory <soakdma37@gmail.com>
2021-10-11 00:51:12 +0900

    alpha beta 프루닝 1차 완성

--- AI_assignment02/hw02.py
+++ AI_assignment02/hw02.py
@@ -60,9 +60,10 @@ class MinimaxAgent(AdversialSearchAgent):
         v = float("-inf")
         move_candidate = gameState.getLegalActions(agentIndex)
         pacmanIndex=0
+        firstGhostIndex=pacmanIndex+1
         # 갈 수 있는 곳 중 점수를 최대화하는 곳으로
         for action in move_candidate:
-            v=max(v, self.minValue(gameState.generateSuccessor(agentIndex, action), depth, pacmanIndex+1))
+            v=max(v, self.minValue(gameState.generateSuccessor(agentIndex, action), depth, firstGhostIndex))
             #팩맨 다음 인덱스부터 시작해서 minvalue 탐색
 
         return v
@@ -91,10 +92,10 @@ class MinimaxAgent(AdversialSearchAgent):
     def Action(self, gameState):
         ####################### Write Your Code Here ################################
         pacmanIndex = 0
-
+        firstGhostIndex = pacmanIndex + 1
         move_candidate = gameState.getLegalActions(pacmanIndex)
 
-        scores = [self.minValue(gameState.generateSuccessor(pacmanIndex, action), 0, pacmanIndex+1) for action in move_candidate]
+        scores = [self.minValue(gameState.generateSuccessor(pacmanIndex, action), 0, firstGhostIndex) for action in move_candidate]
         # 팩맨 다음 인덱스부터 minvalue 계산 시작해야
         #print(scores)
         bestScore = max(scores)
@@ -113,11 +114,80 @@ class AlphaBetaAgent(AdversialSearchAgent):
       [문제 02] AlphaBeta의 Action을 구현하시오. (25점)
       (depth와 evaluation function은 위에서 정의한 self.depth and self.evaluationFunction을 사용할 것.)
     """
+    def maxValue(self, gameState, depth, agentIndex, alpha, beta):
+        if depth == self.depth or gameState.isWin() or gameState.isLose():
+            # terminal of search
+            return self.evaluationFunction(gameState)
+
+        v=float("-inf")
+
+        move_candidate = gameState.getLegalActions(agentIndex)
+        pacmanIndex = 0
+        firstGhostIndex = pacmanIndex + 1
+        # 갈 수 있는 곳 중 점수를 최대화하는 곳으로
+        for action in move_candidate:
+            v = max(v, self.minValue(gameState.generateSuccessor(pacmanIndex, action), depth, firstGhostIndex, alpha, beta))
+            if v>=beta:
+                return v
+            alpha=max(alpha, v)
+
+        return v
+
+    def minValue(self, gameState, depth, agentIndex, alpha, beta):
+        if depth == self.depth or gameState.isWin() or gameState.isLose():
+            # terminal of search
+            return self.evaluationFunction(gameState)
+
+        v = float("inf")
+        move_candidate = gameState.getLegalActions(agentIndex)
+        agentNum = gameState.getNumAgents()
+        pacmanIndex = 0
+        firstGhostIndex = pacmanIndex + 1
+
+        if agentIndex==agentNum-1:
+            for action in move_candidate:
+                # 다음 depth로 넘기고 팩맨의 움직임으로
+
+                v=min(v, self.maxValue(gameState.generateSuccessor(agentIndex, action), depth+1, pacmanIndex, alpha, beta))
+                if v<=alpha:
+                    return v
+                beta=min(beta, v)
+            return v
+        else:
+            for action in move_candidate:
+
+                v=min(v, self.minValue(gameState.generateSuccessor(agentIndex, action), depth, agentIndex+1, alpha, beta))
+                # 다음 에이전트부터 또 시작
+
+                if v<=alpha:
+                    return v
+                beta=min(beta,v)
+            return v
+
+
 
     def Action(self, gameState):
         ####################### Write Your Code Here ################################
 
-        raise Exception("Not implemented yet")
+        pacmanIndex = 0
+        firstGhostIndex=pacmanIndex+1
+        move_candidate = gameState.getLegalActions(pacmanIndex)
+        alpha=float("-inf")
+        beta=float("inf")
+        #value, move=self.maxValue(gameState, 0, pacmanIndex, alpha, beta)
+
+        scores = [self.minValue(gameState.generateSuccessor(pacmanIndex, action), 0, firstGhostIndex, alpha, beta) for action in
+                  move_candidate]
+        # 팩맨 다음 인덱스부터 minvalue 계산 시작해야
+        # print(scores)
+        bestScore = max(scores)
+        Index = [index for index in range(len(scores)) if scores[index] == bestScore]
+        get_index = random.choice(Index)
+
+        return move_candidate[get_index]
+
+
+        #raise Exception("Not implemented yet")
 
         ############################################################################
 
